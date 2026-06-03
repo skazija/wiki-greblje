8a39d38d97238eff5a1522b59f9e98e0fc926e23
Sead Kazija <skazija@yahoo.com>
2026-06-03 19:36:39 +0200

    Fix media urls indentation

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
     path("", include("graves.urls")),
     path("accounts/", include("accounts.urls")),
 ]
-
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
