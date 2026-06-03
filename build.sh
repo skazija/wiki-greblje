861dccdf3c52c6dda26bc1e0fbf1e69c877d01dc
Sead Kazija <skazija@yahoo.com>
2026-06-03 13:50:17 +0200

    Create admin user od deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,16 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+username='admin';
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        skazija,
+        'skazija@yahoo.com',
+        'HamuZUZU2026!'
+    )
