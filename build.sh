383716ca8190fef3556a19291d74166d02e30a44
Sead Kazija <skazija@yahoo.com>
2026-06-03 14:45:41 +0200

    Remove temporary admin creation

--- build.sh
+++ build.sh
@@ -6,15 +6,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-if not User.objects.filter(username='skazija').exists():
-    User.objects.create_superuser(
-        'skazija',
-        'skazija@yahoo.com',
-        'HamuZUZU2026!'
-    )
-"
