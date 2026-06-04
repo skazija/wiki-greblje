9155a4b527eb6b25ab451ed4987c0f0c713ae635
Sead Kazija <skazija@yahoo.com>
2026-06-04 13:03:43 +0200

    Serve media files on Render test deployment

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +27,10 @@ urlpatterns = [
     path("", include("graves.urls")),
     path("accounts/", include("accounts.urls")),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
